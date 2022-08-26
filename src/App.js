9e3575acd64c8842cef075d2be1ad978398459fe
JesseBoe <JesseBoe@gmail.com>
2022-08-26 14:57:06 -0700

    Hash Router for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Link, Route, Redirect } from 'react-router-dom';
 
 import Home from './components/Home/Home.js'
 import BasicOverview from './components/BasicOverview/BasicOverview.js'
