6c51e03f3f2ff22b7a2e387287cc65e98f3b61d4
JesseBoe <JesseBoe@gmail.com>
2022-08-26 15:02:54 -0700

    Updates

---
+++ precache-manifest.a4642ef50087d2a78f885b58ed743a0f.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f6c8cd3921c1d95c86b0c716a8dd63",
+    "url": "/OSFM/index.html"
+  },
+  {
+    "revision": "ad479053632a7a849bc0",
+    "url": "/OSFM/static/css/main.106930bf.chunk.css"
+  },
+  {
+    "revision": "239c930f9ad253edea74",
+    "url": "/OSFM/static/js/2.ab876b88.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/OSFM/static/js/2.ab876b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad479053632a7a849bc0",
+    "url": "/OSFM/static/js/main.a39164bd.chunk.js"
+  },
+  {
+    "revision": "73a0176e5db8cace051c",
+    "url": "/OSFM/static/js/runtime-main.c2b63c86.js"
+  },
+  {
+    "revision": "314fc6e8e8091c39d7ff5c830c9066f7",
+    "url": "/OSFM/static/media/Antidote.314fc6e8.png"
+  },
+  {
+    "revision": "3d5fcbd39c73779273277cd154a806da",
+    "url": "/OSFM/static/media/Basics01.3d5fcbd3.png"
+  },
+  {
+    "revision": "6bc1189cf143b88aa5be571aba7941e1",
+    "url": "/OSFM/static/media/Basics03.6bc1189c.png"
+  },
+  {
+    "revision": "a632f9a1cd975c982cf0c074b2d6f42f",
+    "url": "/OSFM/static/media/Basics04.a632f9a1.png"
+  },
+  {
+    "revision": "655f97ae19bd40264c2985c6f2856e6a",
+    "url": "/OSFM/static/media/BattleBG.655f97ae.png"
+  },
+  {
+    "revision": "04450b4b8c6547611e95a4bfbb70bdd2",
+    "url": "/OSFM/static/media/CustomZone01.04450b4b.png"
+  },
+  {
+    "revision": "8dc06537ad3e491c0f475f03da8c66e6",
+    "url": "/OSFM/static/media/DP1.8dc06537.png"
+  },
+  {
+    "revision": "42745199893e7b697fd1e838f82ff5a6",
+    "url": "/OSFM/static/media/DP2.42745199.png"
+  },
+  {
+    "revision": "89613c675b32e7c78f260e020ef1a25c",
+    "url": "/OSFM/static/media/FC2.89613c67.png"
+  },
+  {
+    "revision": "8aaf2e717c88bc36403cb554140a4e79",
+    "url": "/OSFM/static/media/FE1.8aaf2e71.png"
+  },
+  {
+    "revision": "548939309cfba105c90eb05993913488",
+    "url": "/OSFM/static/media/FE2.54893930.png"
+  },
+  {
+    "revision": "2f1369d7c6db51fd40bbaa89e89be4a7",
+    "url": "/OSFM/static/media/FirstSpell01.2f1369d7.png"
+  },
+  {
+    "revision": "3af296381ba9eda2918806f3c7eba9c1",
+    "url": "/OSFM/static/media/FirstSpell02.3af29638.png"
+  },
+  {
+    "revision": "e191d2f66a045751603c55a39e9f303d",
+    "url": "/OSFM/static/media/FirstSpell03.e191d2f6.png"
+  },
+  {
+    "revision": "83aa729c3efd57ad15ec8a27c5ddc1ba",
+    "url": "/OSFM/static/media/FirstSplash.83aa729c.png"
+  },
+  {
+    "revision": "c5da69f3e328242f77c1a0a468e95e47",
+    "url": "/OSFM/static/media/FolderStructure.c5da69f3.png"
+  },
+  {
+    "revision": "b245fa30912f7151241efb08e915610d",
+    "url": "/OSFM/static/media/GIF.b245fa30.gif"
+  },
+  {
+    "revision": "71efc2909877a3c330862e293ecdcac7",
+    "url": "/OSFM/static/media/Harmony01.71efc290.png"
+  },
+  {
+    "revision": "f010d0ce8f22b4feb5b9f63eb0c69015",
+    "url": "/OSFM/static/media/Harmony02.f010d0ce.png"
+  },
+  {
+    "revision": "0e4e654ce1d71526c74e8ead6b342965",
+    "url": "/OSFM/static/media/Harmony03.0e4e654c.png"
+  },
+  {
+    "revision": "93383d65f20dd554373792070db1a56d",
+    "url": "/OSFM/static/media/Harmony04.93383d65.png"
+  },
+  {
+    "revision": "a755ce9461f8cde818d9a9fe27214f36",
+    "url": "/OSFM/static/media/Harmony05.a755ce94.png"
+  },
+  {
+    "revision": "71f9c2896d07a7c792ab010f36d644d8",
+    "url": "/OSFM/static/media/Harmony06.71f9c289.png"
+  },
+  {
+    "revision": "683f20f1dbc519008deb14acea343749",
+    "url": "/OSFM/static/media/Harmony07.683f20f1.png"
+  },
+  {
+    "revision": "c93d34e2e58030b4fc94b9091dfa403c",
+    "url": "/OSFM/static/media/Ice-bg.c93d34e2.png"
+  },
+  {
+    "revision": "03ce4dbeb7975685fa2fb1b88db64487",
+    "url": "/OSFM/static/media/Ice-bg_0.03ce4dbe.png"
+  },
+  {
+    "revision": "84765613b9ffc77cf6cfbae1e2b91299",
+    "url": "/OSFM/static/media/InitLuaExample.84765613.png"
+  },
+  {
+    "revision": "68f28892e001e18fa0f94616b8f5646b",
+    "url": "/OSFM/static/media/Intoxication.68f28892.png"
+  },
+  {
+    "revision": "9443d741d9b859e4d690af1199dc37c6",
+    "url": "/OSFM/static/media/MLP01.9443d741.png"
+  },
+  {
+    "revision": "382bbb8d14c9038587dd0cbd5948aacd",
+    "url": "/OSFM/static/media/Miniminigun.382bbb8d.png"
+  },
+  {
+    "revision": "4490e48547b3c3b9276d6d660ff378ae",
+    "url": "/OSFM/static/media/MyBrandBG.4490e485.png"
+  },
+  {
+    "revision": "5a0b2eff0dc45d1782e85f83b46a8659",
+    "url": "/OSFM/static/media/OSFMBG1.5a0b2eff.png"
+  },
+  {
+    "revision": "1b80278b722a742b014e9ea772b6ad2b",
+    "url": "/OSFM/static/media/SS1.1b80278b.png"
+  },
+  {
+    "revision": "237a5c37633a41dd73ad217fcae0379d",
+    "url": "/OSFM/static/media/SS2.237a5c37.png"
+  },
+  {
+    "revision": "b7d98c4c187466d20d80c009799dcfc9",
+    "url": "/OSFM/static/media/SS3.b7d98c4c.png"
+  },
+  {
+    "revision": "2e894758ec97ea952b6f98d925a75f53",
+    "url": "/OSFM/static/media/Tools02.2e894758.png"
+  },
+  {
+    "revision": "dbd1bae40b996101e7f6abdeb369e2c4",
+    "url": "/OSFM/static/media/Workshop01.dbd1bae4.png"
+  },
+  {
+    "revision": "1aeab0ad64cf9cbc8e3b523166c586be",
+    "url": "/OSFM/static/media/Workshop02.1aeab0ad.png"
+  },
+  {
+    "revision": "3b288cbd4449c5099942feb971a372a8",
+    "url": "/OSFM/static/media/Workshop03.3b288cbd.png"
+  },
+  {
+    "revision": "295c9dc558b34d86b82720474f2631ff",
+    "url": "/OSFM/static/media/Workshop04.295c9dc5.png"
+  },
+  {
+    "revision": "23f7ab73de26d3055b1f169a6a46a90a",
+    "url": "/OSFM/static/media/Workshop05.23f7ab73.png"
+  },
+  {
+    "revision": "b127fcb0a25d9cabff5c2e1baebd59d6",
+    "url": "/OSFM/static/media/Workshop06.b127fcb0.png"
+  },
+  {
+    "revision": "abe9de89b7002fde271528072d406414",
+    "url": "/OSFM/static/media/WorldIce.abe9de89.png"
+  },
+  {
+    "revision": "59bd0c460a8569606d18f9c8dbb09305",
+    "url": "/OSFM/static/media/ZoneGuide.59bd0c46.png"
+  },
+  {
+    "revision": "2654e003598fabcad8c15221446f11f9",
+    "url": "/OSFM/static/media/bg-1.2654e003.png"
+  },
+  {
+    "revision": "7aa21ec55c0f28f80c59ed81c38eb790",
+    "url": "/OSFM/static/media/fc5.7aa21ec5.png"
+  },
+  {
+    "revision": "4506379fd67d832f241cbd353b182d90",
+    "url": "/OSFM/static/media/i3.4506379f.png"
+  }
+]);
